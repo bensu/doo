b1d817375b3bad2c5e4d3c1c04dc66b36476d6fb
Sebastian Bensusan <sbensu@gmail.com>
2015-05-10 21:03:09 +0200

    WIP: async test make the script terminate before it should

--- test/doo/core-test.cljs
+++ test/doo/core-test.cljs
@@ -3,5 +3,15 @@
 
 (enable-console-print!)
 
-(deftest js-envs 
+(deftest sync-test 
   (is (= 2 1)))
+
+;; Only works in browsers because it uses setTimeout
+(deftest async-test
+  (async done
+    (is (= 1 1))
+    (let [a 1]
+      (js/setTimeout (fn []
+                       (is (= a 2))
+                       (done))
+        500))))
