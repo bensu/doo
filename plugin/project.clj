9564ba322398a42d7300e2879dcc446136b192a2
Sebastian Bensusan <sbensu@gmail.com>
2015-06-18 18:34:43 +0200

    Fixed old dependency

--- plugin/project.clj
+++ plugin/project.clj
@@ -13,7 +13,7 @@
   
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [org.clojure/clojurescript "0.0-3308" :scope "provided"]
-                 [doo "0.1.0-SNAPSHOT"]]
+                 [doo "0.1.1-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]]
 
