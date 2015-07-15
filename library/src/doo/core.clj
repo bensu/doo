11fa6f424c086a017085fb5659e01190afaaaf9f
Sebastian Bensusan <sbensu@gmail.com>
2015-07-15 23:26:53 +0200

    Improved error message when the runner is not installed. Fixes #8

--- library/src/doo/core.clj
+++ library/src/doo/core.clj
@@ -29,28 +29,36 @@
 
 (def base-dir "runners/")
 
-(defn runner-path! [runner filename]
+(defn runner-path!
   "Creates a temp file for the given runner resource file"
+  [runner filename]
   (let [full-path (str base-dir filename)
         runner-path (.getAbsolutePath
-                     (doto (File/createTempFile (name runner) ".js")
-                       (.deleteOnExit)
-                       (#(io/copy (slurp (io/resource full-path)) %))))]
+                      (doto (File/createTempFile (name runner) ".js")
+                        (.deleteOnExit)
+                        (#(io/copy (slurp (io/resource full-path)) %))))]
     runner-path))
 
 (defn get-resource [rs]
   (.getPath (io/resource (str base-dir rs))))
 
+(def command-table
+  {:phantom "phantomjs" 
+   :slimer "slimerjs" 
+   :rhino "rhino"
+   :node "node"})
+
 ;; Define in terms of multimethods to allow user extensibility
 (defn js->command [js]
   {:pre [(keyword? js)]
    :post [(not (nil? %))]}
-  (case js
-    :phantom ["phantomjs" (runner-path! :phantom "unit-test.js") 
-              (runner-path! :phantom-shim "phantomjs-shims.js")]
-    :slimer ["slimerjs" (runner-path! :slimer "unit-test.js") ]
-    :rhino ["rhino" "-opt" "-1" (runner-path! :rhino "rhino.js")]
-    :node ["node" (runner-path! :node "node-runner.js")]))
+  (let [cmd (command-table js)]
+    (case js
+      :phantom [cmd (runner-path! :phantom "unit-test.js") 
+                (runner-path! :phantom-shim "phantomjs-shims.js")]
+      :slimer [cmd (runner-path! :slimer "unit-test.js") ]
+      :rhino [cmd "-opt" "-1" (runner-path! :rhino "rhino.js")]
+      :node [cmd (runner-path! :node "node-runner.js")])))
 
 ;; ====================================================================== 
 ;; Compiler options
@@ -73,11 +81,26 @@
 ;; ====================================================================== 
 ;; Bash
 
+(def cmd-not-found
+  "We tried running %s but we couldn't find it your system. Try:
+\n\t %s \n
+If it doesn't work you need to install %s, see https://github.com/bensu/doo#setting-up-environments\n
+If it does work, file an issue and we'll sort it together!")
+
 (defn run-script
   "Runs the script defined in :output-to of compiler-opts
    and runs it in the selected js-env."
   [js-env script-path]
   {:pre [(valid-js-env? js-env)]}
-  (let [r (apply sh (conj (js->command js-env) script-path))]
-    (println (:out r))
-    r))
+  (try
+    (let [r (apply sh (conj (js->command js-env) script-path))]
+      (println (:out r))
+      r)
+    (catch java.io.IOException e
+      (let [cmd (command-table js-env)
+            error-msg (format cmd-not-found cmd
+                        (if (= js-env :rhino) "rhino -help" (str cmd " -v"))
+                        cmd)]
+        (println error-msg)
+        {:exit 127
+         :out error-msg}))))
