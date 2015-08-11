df83c17dd66ab552a528c8cfe6b752784e3e3baa
Sebastian Bensusan <sbensu@gmail.com>
2015-08-11 22:57:55 +0200

    Bump all projects to account for karma changes

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-doo "0.1.4-SNAPSHOT"
+(defproject lein-doo "0.1.5-SNAPSHOT"
   :description "lein-doo is a plugin to run clj.test on different js environments."
   :url "https://github.com/bensu/doo"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
   
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308" :scope "provided"]
-                 [doo "0.1.4-SNAPSHOT"]]
+                 [doo "0.1.5-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]]
 
