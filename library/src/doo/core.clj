30908dba57cbd11fe7bb77ca03955048d6944a3d
Sebastian Bensusan <sbensu@gmail.com>
2015-08-15 17:32:15 +0200

    Improve error msg for invalid alias

--- library/src/doo/core.clj
+++ library/src/doo/core.clj
@@ -34,11 +34,12 @@
   {:pre [(keyword? js-env)]}
   (contains? js-envs js-env))
 
-(defn assert-alias [js-env-alias js-envs]
-  (assert (not (empty? js-envs))
+(defn assert-alias [js-env-alias resolved-js-envs]
+  (assert (not (empty? resolved-js-envs))
     (str "The given alias: " js-env-alias
       " didn't resolve to any runners. Try any of: "
-       (str/join ", " (keys default-aliases)))))
+       (str/join ", " (map name js-envs)) " or "
+       (str/join ", " (map name (keys default-aliases))))))
 
 (defn assert-js-env
   "Throws an exception if the js-env is not valid.
