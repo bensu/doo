5be47995fe113e8d6cd06b33c3cd1499dda2e7ee
Sebastian Bensusan <sbensu@gmail.com>
2015-10-10 21:29:07 +0200

    Add karma to plugin tests

--- example/script/test-all.sh
+++ example/script/test-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 lein clean
-lein doo phantom test once
-lein doo phantom advanced once
-lein doo phantom none-test once
+lein doo all test once
+lein doo all advanced once
+lein doo all none-test once
 lein doo node node-none once
 lein doo node node-advanced once
