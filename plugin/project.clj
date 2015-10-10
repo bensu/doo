b23e7721cbc3f7665591ba377e59493164124d58
Sebastian Bensusan <sbensu@gmail.com>
2015-10-10 17:23:03 +0200

    Correct doo library version

--- plugin/project.clj
+++ plugin/project.clj
@@ -13,7 +13,7 @@
   
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "0.0-3308" :scope "provided"]
-                 [doo "0.1.5"]]
+                 [doo "0.1.6-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]]
 
