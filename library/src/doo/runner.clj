61bf69b716c9bfe0d05ccf7ae6dc2ebbad4a7156
Sebastian Bensusan <sbensu@gmail.com>
2015-10-11 17:27:39 +0200

    Add doo-all-tests for karma runners
    Bug: the total test count is artificially set to 0

--- library/src/doo/runner.clj
+++ library/src/doo/runner.clj
@@ -5,18 +5,25 @@
 (defmacro count-tests [namespaces]
   `(+ ~@(map (fn [ns]
                (count (filter #(:test (nth % 1) false)
-                        (cljs.analyzer.api/ns-publics (second ns)))))
+                        (cljs.analyzer.api/ns-publics ns))))
           namespaces)))
 
 (defmacro doo-all-tests []
   `(doo.runner/set-entry-point!
-     (fn [] (cljs.test/run-all-tests))))
+     (if (doo.runner/karma?)
+       (fn [tc#]
+         (jx.reporter.karma/start tc# 0)
+         (cljs.test/run-all-tests #".*"
+           (cljs.test/empty-env :jx.reporter.karma/karma)))
+       (fn []
+         (cljs.test/run-all-tests)))))
 
 (defmacro doo-tests [& namespaces]
   `(doo.runner/set-entry-point!
      (if (doo.runner/karma?)
        (fn [tc#]
-         (jx.reporter.karma/start tc# (doo.runner/count-tests ~namespaces))
+         (jx.reporter.karma/start tc#
+           (doo.runner/count-tests ~(map second namespaces)))
          (cljs.test/run-tests
            (cljs.test/empty-env :jx.reporter.karma/karma)
            ~@namespaces))
