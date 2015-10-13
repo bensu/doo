916c7d4148ed9296aac5097c6fcf898b268062b3
Sebastian Bensusan <sbensu@gmail.com>
2015-10-13 16:40:05 +0200

    Bump example cljs version

--- example/project.clj
+++ example/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
