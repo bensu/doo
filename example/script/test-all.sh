e46bc459e633870f1953ed1ebe5e605bf87f191a
Sebastian Bensusan <sbensu@gmail.com>
2015-10-17 15:23:57 +0200

    Add default build cli to test script

--- example/script/test-all.sh
+++ example/script/test-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-lein do clean, doo all test once, doo all advanced once, doo all none-test once, doo node node-none once, doo node node-advanced once
+lein do clean, doo all once, doo all advanced once, doo all none-test once, doo node node-none once, doo node node-advanced once
