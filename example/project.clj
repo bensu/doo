a568f94a159222a6fc6c2752fd0f64fac6a76028
Daniel Compton <desk@danielcompton.net>
2015-10-27 12:34:07 +1300

    Add Circle CI testing for doo
    Circle CI is the easiest CI tool for running browser based tests. They
    already have Chrome installed and you don't need to mess around with
    `xvfb` to get it running.
    
    This commit adds a few things:
    - JVM opts for the project.clj - You need to provide this for Circle
      because they run it on boxes with oodles of RAM, but you're only able
      to access 4GB of it. If you don't specify xmx yourself, then the OS
      will try to use more than 4GB.
    - CI badge - fairly self explanatory.
    - Add NPM package.json for installing node dependencies to example
      project.
    - Add failing cljsbuild configs to the example project to be used for
      testing.
    - Adds a Circle testing script to run the tests and check the output.

--- example/project.clj
+++ example/project.clj
@@ -11,40 +11,76 @@
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-doo "0.1.6-SNAPSHOT"]]
 
-  :source-paths ["src" "test"]
+  :source-paths ["src" "test" "failing-tests"]
 
   :clean-targets ^{:protect false} [:target-path "resources/public/js/" "out"]
-  
+
   :doo {:build "test"
-        :paths {:karma "karma"}
+        :paths {:karma "karma"
+                :slimer "./node_modules/.bin/slimerjs"}
         :alias {:browsers [:chrome :firefox]
                 :all [:browsers :headless]}}
 
+  :jvm-opts ["-Xmx1g"]
+
   :cljsbuild
-  {:builds {:dev {:source-paths ["src"]
-                  :compiler {:output-to "resources/public/js/dev.js"
-                             :main example.core
-                             :optimizations :none}}
-            :test {:source-paths ["src" "test"]
-                   :compiler {:output-to "out/testable.js"
-                              :main 'example.runner
-                              :optimizations :simple}}
-            :advanced {:source-paths ["src" "test"]
-                       :compiler {:output-to "out/testable.js"
-                                  :main "example.runner"
-                                  :optimizations :advanced}}
-            :none-test {:source-paths ["src" "test"]
-                        :compiler {:output-to "out/testable.js"
-                                   :main example.runner
-                                   :source-map true
-                                   :optimizations :none}}
-            :node-none {:source-paths ["src" "test"]
-                        :compiler {:output-to "out/testable.js"
-                                   :main example.runner
-                                   :optimizations :none
-                                   :target :nodejs}}
-            :node-advanced {:source-paths ["src" "test"]
-                            :compiler {:output-to "out/testable.js"
-                                       :main example.runner
-                                       :optimizations :advanced
-                                       :target :nodejs}}}})
+  {:builds {:dev                {:source-paths ["src"]
+                                 :compiler     {:output-to     "resources/public/js/dev.js"
+                                                :main          example.core
+                                                :optimizations :none}}
+            :test               {:source-paths ["src" "test"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          'example.runner
+                                                :optimizations :simple}}
+            :advanced           {:source-paths ["src" "test"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          "example.runner"
+                                                :optimizations :advanced}}
+            :none-test          {:source-paths ["src" "test"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          example.runner
+                                                :source-map    true
+                                                :optimizations :none}}
+            :node-none          {:source-paths ["src" "test"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          example.runner
+                                                :optimizations :none
+                                                :target        :nodejs}}
+            :node-advanced      {:source-paths ["src" "test"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          example.runner
+                                                :optimizations :advanced
+                                                :target        :nodejs}}
+
+            ;; These cljsbuild configs are for CI testing only.
+            :dev-fail           {:source-paths ["src"]
+                                 :compiler     {:output-to     "resources/public/js/dev.js"
+                                                :main          example.core
+                                                :optimizations :none}}
+            :test-fail          {:source-paths ["src" "failing-tests"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          'example.failing-runner
+                                                :optimizations :simple}}
+            :advanced-fail      {:source-paths ["src" "failing-tests"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          "example.failing-runner"
+                                                :optimizations :advanced}}
+            :none-test-fail     {:source-paths ["src" "failing-tests"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          example.failing-runner
+                                                :source-map    true
+                                                :optimizations :none}}
+            :node-none-fail     {:source-paths ["src" "failing-tests"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          example.failing-runner
+                                                :optimizations :none
+                                                :target        :nodejs}}
+            :node-advanced-fail {:source-paths ["src" "failing-tests"]
+                                 :compiler     {:output-to     "out/testable.js"
+                                                :main          example.failing-runner
+                                                :optimizations :advanced
+                                                :target        :nodejs}}
+
+            }
+
+   })
