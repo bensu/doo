4f5bca82cbb8ca3c9da0e0a9a38baf64437b9a97
Miikka Koskinen <miikka.koskinen@iki.fi>
2017-10-02 09:02:59 +0300

    Bump development version to 0.1.9-SNAPSHOT

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,4 +1,4 @@
-(defproject lein-doo "0.1.8"
+(defproject lein-doo "0.1.9-SNAPSHOT"
   :description "lein-doo is a plugin to run clj.test on different js environments."
   :url "https://github.com/bensu/doo"
   :license {:name "Eclipse Public License"
@@ -12,7 +12,7 @@
   :eval-in-leiningen true
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [doo "0.1.8"]]
+                 [doo "0.1.9-SNAPSHOT"]]
 
   :test-paths ["test/clj" "test/cljs"]
 
