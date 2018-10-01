5cc234d2f7e44a9c29fae4e3eee278f1f8ef6d6d
Johannes Lötzsch <git@johannesloetzsch.de>
2018-10-01 17:53:32 +0200

    added "npm install" and $PATH

--- example/script/test-all.sh
+++ example/script/test-all.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
+npm install
+
+export PATH=$PATH:node_modules/.bin/
+export CHROME_BIN=$(which chromium || which chrome)
+
 lein do clean, doo all once, doo nashorn once, doo all advanced once, doo all none-test once, doo node node-none once, doo node node-advanced once
